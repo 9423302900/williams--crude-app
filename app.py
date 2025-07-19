43d61d8d3e88dc6c71f852b80dcba8b5e473de4e
9423302900 <ganeshkoli24@gmail.com>
2025-07-19 22:20:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+#Larry Williams-Style Crude Oil Trading App
 
 
 import streamlit as st import pandas as pd import yfinance as yf from fpdf import FPDF import io import gspread from oauth2client.service_account import ServiceAccountCredentials
