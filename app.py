432f341100569a41f993b5f5582cc12e4254cd63
9423302900 <ganeshkoli24@gmail.com>
2025-07-21 21:50:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-import streamlit as st import pandas as pd import yfinance as yf import requests import datetime import plotly.graph_objects as go
+import streamlit as st 
+import pandas as pd 
+import yfinance as yf 
+import requests 
+import datetime
+import plotly.graph_objects as go
 
 === CONFIG ===
 
